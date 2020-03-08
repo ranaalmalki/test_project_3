f08290baa8284fddd58ee52f2874453667576982
vex-xcc <vex.xcc@gmail.com>
2020-03-08 16:35:11 +0300

    Add Delete Article Route

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,47 +1,114 @@
+// Require necessary NPM packages
 const express = require('express');
+
+// Require Mongoose Model for Article
+const Article = require('../models/article');
+
+// Instantiate a Router (mini app that only handles routes)
 const router = express.Router();
-const mongoose = require('mongoose');
 
-const User = require('../models/User');
-
-router.get('/:userId', async (req, res) => {
-
-    try {
-        const user = await User.findById(req.params.userId);
-        res.status(200).json(user); 
-    } catch (err) {
-         res.status(404).json({message: 'Id not found'});
-    }
-});
-
-router.get('/', (req, res) => {
-  User.find()
+/**
+ * Action:        INDEX
+ * Method:        GET
+ * URI:           /api/articles
+ * Description:   Get All Articles
+ */
+router.get('/api/articles', (req, res) => {
+  Article.find()
   // Return all Articles as an Array
-  .then((allUser) => {
-    res.status(200).json({ users: allUser });
+  .then((allArticles) => {
+    res.status(200).json({ articles: allArticles });
   })
   // Catch any errors that might occur
   .catch((error) => {
     res.status(500).json({ error: error });
   });
-  });
-
-router.post('/', async (req, res) => {
-    
-    const saveUser = new User({
-            name: req.body.name,
-            Password: req.body.Password,
-            phone: req.body.phone,
-            email: req.body.email
-
-    })
-    try {
-        await saveUser.save();
-        res.status(200).json(saveUser); 
-    } catch (err) {
-         res.status(404).json(err);
-    }
 });
- 
 
-  module.exports = router
+/**
+ * Action:        SHOW
+ * Method:        GET
+ * URI:           /api/articles/5d664b8b68b4f5092aba18e9
+ * Description:   Get An Article by Article ID
+ */
+router.get('/api/articles/:id', (req, res) => {
+  Article.findById(req.params.id)
+    .then((article) => {
+      if (article) {
+        res.status(200).json({ article: article });
+      } else {
+        // If we couldn't find a document with the matching ID
+        res.status(404).json({
+          error: {
+            name: 'DocumentNotFoundError',
+            message: 'The provided ID doesn\'t match any documents'
+          }
+        });
+      }
+    })
+    // Catch any errors that might occur
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    })
+});
+
+ /**
+  * Action:       CREATE
+  * Method:       POST
+  * URI:          /api/articles
+  * Description:  Create a new Article
+  */
+ router.post('/api/articles', (req, res) => {
+   Article.create(req.body.article)
+   // On a successful `create` action, respond with 201
+   // HTTP status and the content of the new article.
+   .then((newArticle) => {
+     res.status(201).json({ article: newArticle });
+   })
+   // Catch any errors that might occur
+   .catch((error) => {
+     res.status(500).json({ error: error });
+   });
+ });
+
+  /**
+   * Action:      UPDATE
+   * Method:      PATCH
+   * URI:         /api/articles/5d664b8b68b4f5092aba18e9
+   * Description: Update An Article by Article ID
+   */
+
+   /**
+    * Action:       DESTROY
+    * Method:       DELETE
+    * URI:          /api/articles/5d664b8b68b4f5092aba18e9
+    * Description:  Delete An Article by Article ID
+    */
+   router.delete('/api/articles/:id', (req, res) => {
+    Article.findById(req.params.id)
+    .then((article) => {
+      if (article) {
+        // Pass the result of Mongoose's `.delete` method to the next `.then`
+        return article.remove();
+      } else {
+        // If we couldn't find a document with the matching ID
+        res.status(404).json({
+          error: {
+            name: 'DocumentNotFoundError',
+            message: 'The provided ID Doesn\'t match any documents'
+          }
+        });
+      }
+    })
+    .then(() => {
+      // If the deletion succeeded, return 204 and no JSON
+      res.status(204).end();
+    })
+    // Catch any errors that might occur
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    });
+   });
+
+// Export the Router so we can use it in the server.js file
+module.exports = router;
