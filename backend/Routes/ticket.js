65cf851a1dd4e56ca7ec95dbd5cfc7f5070eaf23
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-14 00:47:18 +0300

    Passing ticket to another Emp

--- backend/Routes/ticket.js
+++ backend/Routes/ticket.js
@@ -6,17 +6,17 @@ const Ticket = require('../models/Ticket');
 
 
 
-
-// create ticket embedded in emp
+//-------------create ticket embedded in emp-------------------
 router.post('/:empId',  (req, res) => {
   // store new ticket in memory with data from request body
   const { TicketType, TicketDescription, TicketState } = req.body
   let ticket = {}
   ticket.TicketType = TicketType,
   ticket.TicketDescription = TicketDescription,
-  ticket.TicketState = TicketState
+  ticket.TicketState = TicketState,
+  ticket.TicketsEmp = [req.params.empId]
   const savedTicket = new Ticket(ticket)
-  savedTicket.save()
+  savedTicket.save()     
   // find emp in db by id and add new ticket
   Emp.findById(req.params.empId,async (error, foundEmp) => {
     try{
@@ -27,9 +27,11 @@ router.post('/:empId',  (req, res) => {
     catch(error){
       res.status(404).json(error);
     }
-  }); });
+  }); 
+});
 
 
+//-------------Get all tickets-------------------
   router.get('/tc', (req, res) => {
     Emp.find({ })
       .populate('Ticket')
@@ -42,6 +44,25 @@ router.post('/:empId',  (req, res) => {
         res.json(Emp);
       });
   });
+
+//-------------Pass ticket to another Emp-------------------
+router.patch('/PassTicket/:TicketId',  (req, res) => {
+
+  Ticket.findById(req.params.TicketId,async (error, foundTicket) => {
+    try{
+      await foundTicket.TicketsEmp.push(req.body.TicketsEmp);
+      foundTicket.save()
+      res.status(200).json(foundTicket.TicketsEmp);
+    }
+    catch(error){
+      res.status(404).json(error);
+    }
+  });
+
+});
+
+
+
 
 
 module.exports = router
