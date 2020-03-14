d182d7e85542e695dd2fbebabb1cd40b58e1c25e
ranaalmalki <ranaalmalki12@gmail.com>
2020-03-14 10:22:03 +0300

    import passport

--- backend/Routes/emp.js
+++ backend/Routes/emp.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+// const passport = require('../config/passport');
 const jwt = require('jsonwebtoken');
 const Emp = require('../models/Emp');
 const config = require('../config/bd');
 const Trans = require('../models/Ticket');
-
 
 
 router.post('/register', (req, res) => {
