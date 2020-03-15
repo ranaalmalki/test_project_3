90e676001482f7ced9f0b7ab2d05f9d3892a3ffc
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-15 14:33:21 +0300

    import Tickets and use it

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,16 +2,38 @@ import React from 'react';
 import Transaction from './component/transaction/transaction'
 import Register from './component/Login/Register';
 import LogIn from './component/Login/LogIn'
+import Tickets from './component/Tickets/Tickets'
 import './App.css';
 
-function App() {
+import apiURL from'./APIconfig';
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      Tickets:[], 
+    };
+    console.log('MY API :  ',apiURL);
+    
+  }
+
+  setTickets = (Tickets) =>{
+    this.setState( {Tickets} );
+  }
+
+
+  render(){
   return (
     <div>
-      <Transaction />
+      <p> Welcome :) ! </p>
+      <Tickets 
+      Tickets={this.state.Tickets} 
+      setTickets={this.setTickets} />
+      {/* <Transaction /> */}
       {/* <Register/> */}
       {/* <LogIn /> */}
     </div>
   );
-}
-
-export default App;
+}}
