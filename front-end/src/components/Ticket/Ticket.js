b962a76230639090a3878a3b3f44bc2ea0b685d6
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-17 00:35:54 +0300

    done with css style

--- front-end/src/components/Ticket/Ticket.js
+++ front-end/src/components/Ticket/Ticket.js
@@ -1,11 +1,5 @@
 //Ticket
 import React from 'react';
-
-       {/* <h4> Type:  {this.props.TicketType} </h4>
-        <sub> State:  {this.props.TicketState} </sub>
-        <p> Description:  {this.props.TicketDescription} </p>
-        -------------------------------------- */}
-
 export default class Ticket extends React.Component{
 
   constructor(props){
@@ -15,7 +9,7 @@ export default class Ticket extends React.Component{
       Fltir:'none', 
     };
   }
-  // To display or not the Ticket Description  
+  // To display or not the ticket description  
   TicketClicked = (e) => {
      if( this.state.Fltir === 'none'){
         this.setState({ 
@@ -32,7 +26,6 @@ export default class Ticket extends React.Component{
         <h1
         onClick={this.TicketClicked}>
           {this.props.TicketState} <span class="shots-number"> {this.props.TicketType} </span>
-        {/* <span class="shots-number">{this.props.TicketType}</span> */}
         </h1>
         <h2 className={`DescriptionFltir-${this.state.Fltir}`}>
           {this.props.TicketDescription}</h2>
