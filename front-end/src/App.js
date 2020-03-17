1411ff907c9c8a658bddf518316eac47511dd24e
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-17 21:03:01 +0300

    resolve ==> ./components/SendTickets/SendTickets

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,7 @@ import Manager from './components/manager/Manager';
 // Components
 import Tickets from './components/Ticket/Tickets'
 import Login from './components/login/Login'
-import SendTickets from './components/Emp_SendTickets/SendTickets'
+import SendTickets from './components/SendTickets/SendTickets'
 export default class App extends React.Component {
   render(){
     console.log('MY API :  ',apiURL);
